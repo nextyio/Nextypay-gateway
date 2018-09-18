c8d5715863110f02d740798ed1d85ba112ccc48f
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-09-18 13:57:42 +0700

    comment test code

--- install.php
+++ install.php
@@ -8,7 +8,7 @@ require_once('lib/nextypayupdatedb.php');
 require_once('lib/nextypaysetup.php');
 
 $DBUSER = "root";
-$DBPASSWORD = "Root123!";
+$DBPASSWORD = "root123";
 $DBNAME = "nextypay";
 $DBHOST = "localhost";
 $npdb = new npdb($DBUSER, $DBPASSWORD, $DBNAME, $DBHOST);
@@ -31,23 +31,19 @@ $_url = $mainnet;
 $_updatedb->set_url($_url);
 $_updatedb->set_connection($npdb);
 $_updatedb->set_includes($_blockchain,$_functions);
-//$_updatedb->init_blocks_table_db(3780307);
-//$_updatedb->init_blocks_table_db(7158740);
-//$_updatedb->init_blocks_table_db(7168740);
-//$_updatedb->init_blocks_table_db(7164330);
 $_updatedb->init_blocks_table_db(0);
 //TESTING
 
 //Add merchant
-
+/*
 $wallet = "0xBF878162F34A11c832339ADB0CcCdDb1b091C1E5";
 $name = "test merchant name";
 $url = "test merchant url";
 $tokenKey = "test Token";
 $_updatedb->addMerchant($wallet, $name, $url, $tokenKey);
-
+*/
 //Add request
-
+/*
 $extraData = "0x0aaa";
 $callbackUrl = "google.com";
 $returnUrl = "google.com.vn";
@@ -64,6 +60,6 @@ $fromWallet = "";
 $toWallet = "0xBF878162F34A11c832339ADB0CcCdDb1b091C1E5";
 $wallet = "0xBF878162F34A11c832339ADB0CcCdDb1b091C1E5";
 $_updatedb->addRequest($shopId, $orderId, $extraData, $callbackUrl, $returnUrl, $ntyAmount, $minBlockDistance, $startTime, $endTime, $fromWallet, $toWallet, $wallet );
-
+*/
 //3778554
 ?>
