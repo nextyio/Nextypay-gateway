02c3a4e1ccab95c1063937043641e36335de4a60
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-09-18 14:08:38 +0700

    add jquery and pending payment script

---
+++ template/js/user.js
@@ -0,0 +1,37 @@
+<script type="text/javascript">
+function countInSecond(startTime,endTime) {
+  var timeDiff = endTime - startTime; //in ms
+  // strip the ms
+  timeDiff /= 1000;
+
+  // get seconds
+  var seconds = Math.round(timeDiff);
+  return seconds;
+}
+
+function call_ajax(startTime,reqId,timeout,interval){
+
+	var seconds=countInSecond(startTime,new Date());
+	console.log(seconds, reqId);
+	if (seconds>timeout) {
+		console.log("time out");
+		return;
+	}
+	var paid="0";
+	setTimeout(function(){
+        $.post("ajax.php",
+        {
+            'reqId': reqId
+        },
+        function(data, status){
+            //alert("Data: " + data + "\nStatus: " + status);
+            if ((status == 'success') && (data)) {window.location.replace(data);} else
+            {
+              call_ajax(startTime,reqId,timeout,interval);
+            }
+        });
+	}, interval*1000);
+}
+
+call_ajax(new Date(), <?php echo $reqId; ?>,600,3 );
+</script>
