79a3929a3053da02f9778e9789daaea2d4927625
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-10 12:08:26 +0700

    fix ui

--- register.php
+++ register.php
@@ -1,12 +1,6 @@
 <?php
     require_once('template/header.html');
-?>
-
-    <link href="template/css/register.css" rel="stylesheet" >
-<?php
     require_once('setting.php');
     require_once('template/register.html'); 
-
-    
     require_once('template/footer.html')
 ?>
