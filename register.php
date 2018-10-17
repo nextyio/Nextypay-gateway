c567bfdb000407e146d5410661dd24a369e009d2
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-17 23:29:31 +0700

    allown cors

--- register.php
+++ register.php
@@ -1,5 +1,8 @@
 <?php
     header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Credentials: true ");
+    header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+    header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
     require_once('template/header.html');
     require_once('setting.php');
     require_once('template/register.html'); 
