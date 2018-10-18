1b93b72167675d18bc7d060413b032b1c008b540
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-18 15:32:08 +0700

    update ui, add ref links

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+    require_once('template/html/header.html');
+    require_once('setting.php');
+    require_once('template/html/login.html'); 
+    require_once('template/html/footer.html')
+?>
