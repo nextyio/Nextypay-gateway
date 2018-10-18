b629d7f76a078181b9d69c65aa3c81e317f80984
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-18 14:14:26 +0700

    use basic carousel template of bootstrap3

--- register.php
+++ register.php
@@ -3,8 +3,8 @@
     header("Access-Control-Allow-Credentials: true ");
     header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
     header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
-    require_once('template/header.html');
+    require_once('template/html/header.html');
     require_once('setting.php');
     require_once('template/register.html'); 
-    require_once('template/footer.html')
+    require_once('template/html/footer.html')
 ?>
