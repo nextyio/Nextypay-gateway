64fb257060efba92593523a3a607871ce272e045
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-19 09:49:41 +0700

    fix html path, add basic files for login system

--- register.php
+++ register.php
@@ -5,6 +5,6 @@
     header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
     require_once('template/html/header.html');
     require_once('setting.php');
-    require_once('template/register.html'); 
+    require_once('template//html/register.html'); 
     require_once('template/html/footer.html')
 ?>
