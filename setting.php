50d04d730df4b4be719682b40151b59d5c523c89
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-22 16:20:37 +0700

    off debug mode

--- setting.php
+++ setting.php
@@ -1,7 +1,7 @@
 <?php
-//error_reporting(E_ERROR | E_PARSE);
-ini_set('display_errors',1);
-error_reporting(E_ALL);
+error_reporting(E_ERROR | E_PARSE);
+//ini_set('display_errors',1);
+//error_reporting(E_ALL);
 //merchants register validator
 $gatewayWallet = '0x6f53c8502bb884775e422c7c34be681554cee2ba';
 
