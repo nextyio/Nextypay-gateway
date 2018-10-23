73fcbc59a1e9cbadfec21f4cb5321d685c5a1aa6
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-23 16:26:41 +0700

    test router

--- router/vtex/index.php
+++ router/vtex/index.php
@@ -2,4 +2,8 @@
     foreach ($_GET as $key => $value) {
         echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>"; //TEST
     }
+    $request = $_GET['request'];
+    $arr = explode("/vtex/", $request);
+    $request = $arr[1];
+    echo "<br>$request";
 ?>
