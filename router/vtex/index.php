6d8c2e31d5bb690f51df873a53665c1b6baddfee
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-23 16:38:22 +0700

    vtex payment_methods request

--- router/vtex/index.php
+++ router/vtex/index.php
@@ -1,9 +1,18 @@
 <?php 
+
+    require_once ('json_response.php');
+
     foreach ($_GET as $key => $value) {
-        echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>"; //TEST
+        //echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>"; //TEST
     }
     $request = $_GET['request'];
     $arr = explode("/vtex/", $request);
     $request = $arr[1];
-    echo "<br>$request";
+
+    function getPaymentMethods() {
+        $arr = array("paymentMethods" : array("Nextypay"));
+        json_response($arr, 200);
+    }
+
+    if ($request == 'payment-methods') {getPaymentMethods(); exit;}
 ?>
