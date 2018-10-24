2ea858192342b6b372b64b5ec93093933c72e088
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-24 14:07:30 +0700

    fix bug with absolute paths

--- api/paymentmethods.php
+++ api/paymentmethods.php
@@ -1,7 +1,7 @@
 <?php 
     function paymentMethods() {
         $arr = array("paymentMethods" => array("Diners", "Elo", "Nextypay" ));
-        echo json_response($arr, 200);
+        echo json_encode($arr);
     }
 
     paymentMethods();
