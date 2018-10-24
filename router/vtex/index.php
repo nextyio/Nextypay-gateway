21932b102b8488811dbf4c55e1d5f358b3ab419e
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-24 14:56:46 +0700

    dfshdfs

--- router/vtex/index.php
+++ router/vtex/index.php
@@ -1,16 +1,16 @@
 <?php 
 // header('Content-Type: application/json');
 // header('Status: 200 OK');
-    require_once ('json_response.php');
-    foreach ($_GET as $key => $value) {
-        //echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>"; //TEST
-    }
-    $request = $_GET['request'];
-    $arr = explode("/vtex/", $request);
-    $request = $arr[1];
-    function getPaymentMethods() {
-        $arr = array("paymentMethods" => array("Diners", "Elo" ));
-        echo json_response($arr, 200);
-    }
-    if ($request == 'payment-methods') {getPaymentMethods(); exit;}
+require_once ('json_response.php');
+foreach ($_GET as $key => $value) {
+    //echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>"; //TEST
+}
+$request = $_GET['request'];
+$arr = explode("/vtex/", $request);
+$request = $arr[1];
+function getPaymentMethods() {
+    $arr = array("paymentMethods" => array("Diners", "Elo", "Nextypay" ));
+    echo json_response($arr, 200);
+}
+if ($request == 'payment-methods') {getPaymentMethods(); exit;}
 ?>
