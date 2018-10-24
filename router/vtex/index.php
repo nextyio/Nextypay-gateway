e0f5f700923c6f3f291f45bb08e775475561eb82
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-24 14:30:15 +0700

    revert

--- router/vtex/index.php
+++ router/vtex/index.php
@@ -1,32 +1,16 @@
 <?php 
-    require_once __DIR__ . '/../../api/json_response.php';
+// header('Content-Type: application/json');
+// header('Status: 200 OK');
+    require_once ('json_response.php');
+    foreach ($_GET as $key => $value) {
+        //echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>"; //TEST
+    }
     $request = $_GET['request'];
     $arr = explode("/vtex/", $request);
     $request = $arr[1];
-    //https://{providerBaseUrl}/payment-methods
-    //https://{providerBaseUrl}/payments
-    //https://{providerBaseUrl}/payments/{paymentId}/cancellations
-    //https://{providerBaseUrl}/payments/{paymentId}/settlements
-    //https://{providerBaseUrl}/payments/{paymentId}/refunds
-
-    function getRequestHeaders() {
-        $headers = array();
-        foreach($_SERVER as $key => $value) {
-            if (substr($key, 0, 5) <> 'HTTP_') {
-                continue;
-            }
-            $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-            $headers[$header] = $value;
-        }
-        return $headers;
+    function getPaymentMethods() {
+        $arr = array("paymentMethods" => array("Diners", "Elo", "Nextypay" ));
+        echo json_response($arr, 200);
     }
-    $mid = '1';
-    $pKey = 'fdsgfds';
-    $headers = getRequestHeaders();
-    $params['reqCode'] = 'xxxx';
-    if ($request == 'payment-methods') {        $arr = array("paymentMethods" => array("Diners", "Elo", "Nextypay" ));
-        echo json_response($arr, 200);exit;};
-    if ($request == 'payments') {$params = $_POST; $params['reqCode'] = '1000';};
-    require_once (__DIR__ . '/../../api/filter.php');
-    
+    if ($request == 'payment-methods') {getPaymentMethods(); exit;}
 ?>
